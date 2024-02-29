f70a53df8b1258f3fd86ec0edb63cf8291ec6673
Yev Albano <84282489+AllenLou4@users.noreply.github.com>
2024-02-29 22:18:22 +0800

    ALL

---
+++ .dart_tool/flutter_build/945d02f3aaec80dafa3640bf44deae28/native_assets.d
@@ -0,0 +1,1 @@
+ D:\\Projects\\Codes\\cookie_jar\\.dart_tool\\flutter_build\\945d02f3aaec80dafa3640bf44deae28\\native_assets.yaml: 
---
+++ android/app/src/main/kotlin/com/example/cookie_jar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cookie_jar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
